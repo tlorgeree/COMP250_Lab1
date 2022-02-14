f91ad7d7cd5806b167e7c587ad624a239e883143
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2022-02-14 13:22:14 -0500

    Step 2 Complete

--- src/MessageQueue.java
+++ src/MessageQueue.java
@@ -1,2 +1,36 @@
 public class MessageQueue {
+    boolean inUse = false;
+    int size = 0;
+    MessageFragment head = null;
+    MessageFragment tail = null;
+    String sendReceive;//names od sender and recipient
+
+    public void clear(){
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    public String getStatus(int num){
+        if (inUse) return "Queue #"+ num
+        +": Available";
+        else return "Queue #"+ num
+                +": Unavailable.\n" + size
+                + " characters remaining.";
+    }
+
+    public MessageFragment poll(){
+        MessageFragment currHead = null;
+        if (size == 0){
+            System.out.println("Queue is empty");
+        }else{
+            currHead = head;
+            head = head.nextFragment;
+            --size;
+        }
+        return currHead;
+    }
+
+
+
 }
