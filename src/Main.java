46b208f67c7eb92941a3b25cd9a6306a6787addf
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2022-02-16 15:43:23 -0500

    Added a couple cases

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,32 @@ public class Main {
 
         System.out.println("Please enter your message:\n>>>");
         String message = new Scanner(System.in).nextLine(); //3. Store user input message
-        queue.enqueue(message, "From [Sender] to [Recipient]."); //4. call enqueue()
+        while(message.length() == 0){//Don't accept empty message
+            System.out.println("Message was empty. Please enter your message.");
+            message = new Scanner(System.in).nextLine(); //3. Store user input message
+        }
+        System.out.println("Please enter the name of the Sender:\n>>>");
+        String sender = new Scanner(System.in).nextLine();
+        while(sender.length() == 0){//Don't accept empty Sender
+            System.out.println("Sender must be declared. Please enter the Sender.");
+            sender = new Scanner(System.in).nextLine(); //3. Store user input message
+        }
+        System.out.println("Please enter the name of the Recipient:\n>>>");
+        String recipient = new Scanner(System.in).nextLine();
+        while(recipient.length() == 0){//Don't accept empty Recipient
+            System.out.println("Recipient must be declared. Please enter the Sender.");
+            recipient = new Scanner(System.in).nextLine(); //3. Store user input message
+        }
+        queue.enqueue(message, "From " + sender + " to "+recipient+"."); //4. call enqueue()
         System.out.println(queue.getStatus(0));//5. Print new status
-        if (queue.size >0) {//in case the use passed an empty message earlier
-            System.out.println(queue.head.letter);//6.Print the first and last letter of the queue
-            while (queue.size > 0) {
-                MessageFragment curr_char = queue.poll();
-                if (queue.size == 0) System.out.println(curr_char.letter);
-            }
-        }else System.out.println("The queue is empty, no characters to return.");
+
+        System.out.println(queue.head.letter);//6.Print the first and last letter of the queue
+        while (queue.size > 0) {
+            MessageFragment curr_char = queue.poll();
+            if (queue.size == 0) System.out.println(curr_char.letter);
+        }
+        System.out.println("Message sent from " + sender + " to "+recipient+".");
+
 
     }
 }
